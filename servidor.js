aaad05787ec1053069e2de52fb2bcbc022dc8fff
Paulo <prs.paulorenato@gmail.com>
2025-05-21 22:25:19 -0300

    first commit

---
+++ servidor.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Middleware para servir arquivos estáticos
+app.use(express.static(__dirname));
+
+// Rota para página principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Rota para página de agradecimento
+app.get('/obrigado.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'obrigado.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+}); 
