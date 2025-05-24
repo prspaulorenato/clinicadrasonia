ab1a632e6ec39fd3e706761e5ed84fa82aabc747
Paulo <prs.paulorenato@gmail.com>
2025-05-24 17:21:07 -0300

    alterado codigo para direcionamento para página de obrigado

--- servidor.js
+++ servidor.js
@@ -3,19 +3,29 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware para servir arquivos estáticos
-app.use(express.static(__dirname));
+// Servir arquivos estáticos do diretório raiz
+app.use(express.static(path.join(__dirname)));
 
-// Rota para página principal
+// Rota para a página principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Rota para página de agradecimento
+// Rota para a página de agradecimento
 app.get('/obrigado.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'obrigado.html'));
+  res.sendFile(path.join(__dirname, 'obrigado.html'), (err) => {
+    if (err) {
+      console.error('Erro ao enviar obrigado.html:', err);
+      res.status(404).send('Página não encontrada');
+    }
+  });
+});
+
+// Rota genérica para 404
+app.use((req, res) => {
+  res.status(404).send('Página não encontrada');
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
-}); 
+});
